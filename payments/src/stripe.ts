a1c44ad8067c18b84b6b6b6d6c100cfe066fb20b
dmaizel <dmaizel@redhat.com>
2020-09-27 14:29:28 +0300

    das

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,5 +1,8 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_KEY!, {
-  apiVersion: "2020-08-27",
-});
+export const stripe = new Stripe(
+  "sk_test_51HVcelLRSGIwMFan0rXGm3LesE1LtyjKKulm5zpXu6N1f5u7i6Z7I15QREOlVNU6Y22XdGcyvC0aMdSkL151b1TP005MyKOEtc",
+  {
+    apiVersion: "2020-08-27",
+  }
+);
